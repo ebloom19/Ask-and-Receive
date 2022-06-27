0442afc7da94eb3f2d11c403374b250b0958e85e
Ethan Bloom <ebloom19@gmail.com>
2022-06-27 13:54:02 +0800

    added instance for 1 bedroom house

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -171,9 +171,11 @@ class ScraperController extends Controller
 
                     if($this->numberOfBeds >= 5) {
                         $this->numberOfBeds = "5+";
-                    } elseif(!isset($this->numberOfBeds) || ($this->numberOfBeds == 1 && !isset($this->searchTerms['unitNumber']))) {
+                    } elseif(!isset($this->numberOfBeds)) {
                         $this->numberOfBeds = "ALL";
-                    }
+                    } elseif($this->numberOfBeds == 1 && !isset($this->searchTerms['unitNumber']) {
+                        $this->numberOfBeds = "ALL";
+                    } 
 
                     if(isset($this->searchTerms['unitNumber'])) {
                         $this->investorMetrics = $response["{$suburb}-{$this->searchTerms['postCode']}"]["property_types"]["UNIT"]["bedrooms"][$numberOfBeds];
