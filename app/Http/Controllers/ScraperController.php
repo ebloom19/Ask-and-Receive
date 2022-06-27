b07519b0a418a2adb9c56f138813015fa8a8fd06
Ethan Bloom <ebloom19@gmail.com>
2022-06-27 14:11:35 +0800

    debugging line

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -213,7 +213,7 @@ class ScraperController extends Controller
         // Remove broken info 
 
 
-        // return $numberOfBeds;
+        return "{$investorMetrics} AND {$numberOfBeds}";
         // return $propertyData;
 
         return view('welcome', compact('propertyData', 'states', 'streetTypes', 'investorMetrics', 'numberOfBeds', 'searchTerms'));
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,9 +181,9 @@
               
                                 @if (isset($investorMetrics['investor_metrics']))      
                                     <p class="card-subtitle mb-2 text-muted">Scarborough View: <small> - for {{$numberOfBeds}} Bedroom {{isset($searchTerms['unitNumber']) ? 'Units' : 'Homes'}}</small></p>
-                                    <p class="card-text">Average Sale Price : ${{ trim(strrev(chunk_split(strrev($investorMetrics['investor_metrics']['median_sold_price']), 3, ' '))) }}</p>
+                                    <p class="card-text">Average Sale Price : ${{ trim(strrev(chunk_split(strrev($investorMetrics['investor_metrics']['median_sold_price']), 3, ','))) }}</p>
                                     <p class="card-text">Average Sale Price has risen by {{ round($percentage, 2) }}%</p>
-                                    <p class="card-text">Average Rent : ${{ $investorMetrics['investor_metrics']['median_rental_price'] }}</p>
+                                    <p class="card-text">Average Rent : ${{ $investorMetrics['investor_metrics']['median_rental_price'] }} Weekly</p>
                                     <p class="card-text">Rental Yeild : {{ round((float)$investorMetrics['investor_metrics']['rental_yield'] * 100) }}%</p>
                                 @endif
                                 
