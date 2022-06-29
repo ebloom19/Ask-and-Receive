0e048e733f6808703f017f788ea5de4ecbd0e085
Ethan Bloom <ebloom19@gmail.com>
2022-06-29 17:38:20 +0800

    request change from axios to fetch

--- resources/js/FormMixin.js
+++ resources/js/FormMixin.js
@@ -16,10 +16,13 @@ export default {
           this.loaded = false;
           this.success = false;
           this.errors = {};
-          axios.post(this.action, this.fields).then(response => {
-            this.fields = {}; //Clear input fields.
-            this.loaded = true;
-            this.success = true;
+          fetch(this.action, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: this.fields,
           }).catch(error => {
             this.loaded = true;
             if (error.response.status === 422) {
@@ -30,3 +33,15 @@ export default {
       },
     },
   }
+
+
+//   axios.post(this.action, this.fields).then(response => {
+//     this.fields = {}; //Clear input fields.
+//     this.loaded = true;
+//     this.success = true;
+//   }).catch(error => {
+//     this.loaded = true;
+//     if (error.response.status === 422) {
+//       this.errors = error.response.data.errors || {};
+//     }
+//   });
