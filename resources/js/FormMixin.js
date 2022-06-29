d036bfb4f4be4bbe9a6b067bb11051f2bc1c9dd6
Ethan Bloom <ebloom19@gmail.com>
2022-06-29 17:44:39 +0800

    back to axios and modified route

--- resources/js/FormMixin.js
+++ resources/js/FormMixin.js
@@ -16,13 +16,10 @@ export default {
           this.loaded = false;
           this.success = false;
           this.errors = {};
-          fetch(this.action, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: this.fields,
+          axios.post(this.action, this.fields).then(response => {
+            this.fields = {}; //Clear input fields.
+            this.loaded = true;
+            this.success = true;
           }).catch(error => {
             this.loaded = true;
             if (error.response.status === 422) {
@@ -33,15 +30,3 @@ export default {
       },
     },
   }
-
-
-//   axios.post(this.action, this.fields).then(response => {
-//     this.fields = {}; //Clear input fields.
-//     this.loaded = true;
-//     this.success = true;
-//   }).catch(error => {
-//     this.loaded = true;
-//     if (error.response.status === 422) {
-//       this.errors = error.response.data.errors || {};
-//     }
-//   });
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', [ScraperController::class, 'index']);
 
 // Route::get('/saveItemRoute', [ScraperController::class, 'results'])->name('results');
 
-Route::get('/results', [ScraperController::class, 'results'])->name('results');
+Route::get('/results', 'ScraperController@results');
 
 // Route::get('/submit', 'App\Http\Controllers\FormController@submit');
 
