da5d66c22370bf93824c20ae771cd66847fba0ac
Ethan Bloom <ebloom19@gmail.com>
2022-06-30 19:45:01 +0800

    vue form restructure

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -5,35 +5,35 @@
                  :on-cancel="onCancel"
                  :is-full-page="fullPage"/> -->
 
-        <form @submit.prevent="submit">
+        <form @submit.prevent="login" @keydown="form.onKeydown($event)">
             <div class="formMain">
-                <input type="text" name="streetNumber" placeholder="Street Number">
-                <div v-if="errors && errors.streetNumber" class="text-danger">{{ errors.streetNumber[0] }}</div>
-                <input type="number" name="unitNumber" placeholder="Unit Number">
-                <div v-if="errors && errors.unitNumber" class="text-danger">{{ errors.unitNumber[0] }}</div>
-                <input type="text" name="streetName" placeholder="Street Name">
-                <div v-if="errors && errors.streetName" class="text-danger">{{ errors.streetName[0] }}</div>
-                <select name="streetType">
+                <input type="text" name="streetNumber" v-model="form.streetNumber" placeholder="Street Number">
+                <div v-if="form.errors.has('streetNumber')" v-html="form.errors.get('streetNumber')" />
+                <input type="number" name="unitNumber" v-model="form.unitNumber" placeholder="Unit Number">
+                <div v-if="form.errors.has('unitNumber')" v-html="form.errors.get('unitNumber')" />
+                <input type="text" name="streetName" v-model="form.streetName" placeholder="Street Name">
+                <div v-if="form.errors.has('streetName')" v-html="form.errors.get('streetName')" />
+                <select name="streetType" v-model="form.streetType">
                     <option v-for="item in streetTypes" :key="item">
                         {{ item }}
                     </option>
                 </select>
-                <div v-if="errors && errors.streetType" class="text-danger">{{ errors.streetType[0] }}</div>
+                <div v-if="form.errors.has('streetType')" v-html="form.errors.get('streetType')" />
             </div>
 
             <div class="formMain">
-                <input type="text" name="suburb" placeholder="Suburb">
-                <div v-if="errors && errors.suburb" class="text-danger">{{ errors.suburb[0] }}</div>
+                <input type="text" name="suburb" v-model="form.suburb" placeholder="Suburb">
+                <div v-if="form.errors.has('suburb')" v-html="form.errors.get('suburb')" />
 
-                <select name="state">
+                <select name="state" v-model="form.state">
                     <option v-for="st in states" :key="st" >{{ st }}</option>
                 </select>
-                <div v-if="errors && errors.state" class="text-danger">{{ errors.state[0] }}</div>
+                <div v-if="form.errors.has('state')" v-html="form.errors.get('state')" />
 
-                <input type="number" name="postCode" placeholder="Post Code">
-                <div v-if="errors && errors.postCode" class="text-danger">{{ errors.postCode[0] }}</div>
+                <input type="number" name="postCode" v-model="form.postCode" placeholder="Post Code">
+                <div v-if="form.errors.has('postCode')" v-html="form.errors.get('postCode')" />
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" :disabled="form.busy" class="btn btn-primary">Submit</button>
 
                 <div v-if="success" class="alert alert-success mt-3">
                     Message sent!
@@ -48,6 +48,7 @@
     import Loading from 'vue-loading-overlay';
     import 'vue-loading-overlay/dist/vue-loading.css';
     import '../../css/app.css';
+    import Form from 'vform';
 
     export default {
         data() {
@@ -56,11 +57,15 @@
                 fullPage: true,
                 streetTypes: ['Alley', 'Arcade', 'Avenue', 'Boulevard', 'Bypass', 'Circuit', 'Close', 'Corner', 'Court', 'Crescent', 'Cul-de-sac', 'Drive', 'Esplanade', 'Green', 'Grove', 'Highway', 'Junction', 'Lane', 'Link', 'Mews', 'Parade', 'Place', 'Ridge', 'Road', 'Square', 'Street', 'Terrace'],
                 states: ["NSW", "VIC", "QLD", "TAS", "SA", "WA", "NT", "ACT"],
-                action: '/results',
-                fields: {},
-                errors: {},
-                success: false,
-                loaded: true,
+                form: new Form({
+                    streetNumber: '',
+                    unitNumber: '',
+                    streetName: '',
+                    streetType: '',
+                    suburb: '',
+                    state: '',
+                    postCode: ''
+                }),
             }
         },
         components: {
@@ -77,23 +82,9 @@
             onCancel() {
                 console.log('User cancelled the loader.')
             },
-            submit() {
-                if (this.loaded) {
-                this.loaded = false;
-                this.success = false;
-                this.errors = {};
-                axios.post(this.action, this.fields).then(response => {
-                    this.fields = {}; //Clear input fields.
-                    this.loaded = true;
-                    this.success = true;
-                }).catch(error => {
-                    this.loaded = true;
-                    if (error.response.status === 422) {
-                    this.errors = error.response.data.errors || {};
-                    }
-                });
-                }
-            },
+            async login () {
+                const response = await this.form.post('/results');
+            }
         }
     }
     
